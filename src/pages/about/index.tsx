b2d5216590d067d467615a6d364f939bd578463a
Petar Avramovic <petar.lg7@gmail.com>
2021-10-26 17:41:16 +0200

    Importing pictures and sorting texts in Home/About

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,35 @@
+import { Box, Card, CardMedia, Container, Grid, Typography } from '@mui/material';
 import React from 'react';
 
 const About = () => {
     return (
-        <>About.</>
+        <Container>
+            <Box m={2} pt={3} px={2} sx={{ bgcolor: '#cfe8fc', height: '500px', width: "100%" }}>
+                    <Grid container spacing ={2}>
+                        <Grid item xs={6}>
+                            <Card>
+                                <CardMedia 
+                                    component="img"
+                                    image={"/images/about_pic.jpg"}
+                                    alt="photo"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography align="center" borderBottom={1}>
+                                <h1>About me</h1>
+                            </Typography>
+                            <Typography>
+                                <p>I'm 32 year old self taught artist,  based on the south coast of England, who has always had a passion for art.</p>
+                                <p>I started my venture into art at a very young age and have continued to follow my passion and love for all things art into my adult years. </p>
+                                <p>Unlike many artists, I don't have a preferred subject or medium that I like to follow. To me, art is about the consistent growth of one's skills, and always looking to improve, and take on new challenges.</p>
+                                <p>I offer a wide array of art work, from pet portraiture, wild life, abstracts , digital art , landscapes and botanicals. </p>
+                                <p>To me, there's never a "fail" in an art piece, as for every hiccup, there's beauty and something to learn from each new experience. </p>
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Box>
+        </Container>
     );
 };
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,92 +3,97 @@ import React from 'react';
 
 const Home = () => {
     return (
-            <Container>
-                <Box m={2} pt={3} px={2} sx={{ bgcolor: '#cfe8fc', height: '400px', width: "100%" }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <Typography align="center" borderBottom={1}>
-                                <h1>Pet Portraits & Commissions</h1>
-                            </Typography>
-                            <Typography>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Card>
-                                <CardMedia 
-                                    component="img"
-                                    image={"https://scontent.fbeg1-1.fna.fbcdn.net/v/t1.6435-9/241813029_4851381168313141_1523254842365993390_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=a26aad&_nc_eui2=AeFcPr5tm1sGSE40M_y5qAttp42ACN6CUZinjYAI3oJRmMzkkos_EBdb5waLU5rY0rPGAVYPUw-DaejmRc8ddxTy&_nc_ohc=tnCmVpAxAo4AX9EMtKo&_nc_ht=scontent.fbeg1-1.fna&oh=c79d2a00ab3e867184ed672a55bf49ae&oe=616BDE9D"}
-                                    alt="photo"
-                                />
-                            </Card>
-                        </Grid>
+        <Container>
+            <Box m={2} pt={3} px={2} sx={{ bgcolor: '#cfe8fc', height: '500px', width: "100%" }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Typography align="center" borderBottom={1}>
+                            <h1>Pet Portraits & Commissions</h1>
+                        </Typography>
+                        <Typography>
+                            <p>I adore drawing customers beloved pets, and seeing them come to life on the paper.</p>
+                            <p>A pet portrait is a unique and special gift, and a wonderful way to capture a treasured memory to keep your pet with you forever.</p>
+                            <p>I offer a wide variety of sizes, both framed and unframed options are available. Price range starts from £40 upwards so there is an option available for everyone. </p>
+                        </Typography>
                     </Grid>
-                </Box>
-                <Box m={2} pt={3} px={2} sx={{ bgcolor: '#cfe8fc', height: '400px', width: "100%" }}>
-                    <Grid container spacing ={2}>
-                        <Grid item xs={6}>
-                            <Card>
-                                <CardMedia 
-                                    component="img"
-                                    image={"https://scontent.fbeg1-1.fna.fbcdn.net/v/t1.6435-9/209559220_4625822674202326_5716549258690711660_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=a26aad&_nc_eui2=AeEFHbgGQ4AaVAKUQ1eKlfmQjqHoZbQwo9qOoehltDCj2vsLKnghvfbnn8SgJSDvyz1DTXGEeue1vhiCysLfFrFd&_nc_ohc=kPAnpEJmguwAX9K55Bg&_nc_ht=scontent.fbeg1-1.fna&oh=ece283053438816661cf647b7df09e78&oe=616D3827"}
-                                    alt="photo"
-                                />
-                            </Card>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography align="center" borderBottom={1}>
-                                <h1>Original Artwork</h1>
-                            </Typography>
-                            <Typography>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </Typography>
-                        </Grid>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                height="500px"
+                                image={"/images/Pets/17846820302631474.jpg"}
+                                alt="photo"
+                            />
+                        </Card>
                     </Grid>
-                </Box>
-                <Box m={2} pt={3} px={2} sx={{ bgcolor: '#cfe8fc', height: '400px', width: "100%" }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <Typography align="center" borderBottom={1}>
-                                <h1>Gallery</h1>
-                            </Typography>
-                            <Typography>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Card>
-                                <CardMedia 
-                                    component="img"
-                                    image={"https://scontent.fbeg1-1.fna.fbcdn.net/v/t1.6435-9/232396320_4729118877206038_9067115924514914984_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=a26aad&_nc_eui2=AeGp0eUX1gIaIp1MUX43j8hc9C3r4-EMNTX0Levj4Qw1NVUL0WPSBMBE6YR70kR9nRw8l9e11MFZxbgjMsbBLR-l&_nc_ohc=ZcS7P7qEqywAX_TtmMA&_nc_ht=scontent.fbeg1-1.fna&oh=af07bbdf8934cba8fd862abc9b9032a0&oe=616D4F9E"}
-                                    alt="photo"
-                                />
-                            </Card>
-                        </Grid>
+                </Grid>
+            </Box>
+            <Box m={2} pt={3} px={2} sx={{ bgcolor: '#cfe8fc', height: '500px', width: "100%" }}>
+                <Grid container spacing ={2}>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardMedia 
+                                component="img"
+                                image={"/images/Abstract/Neptune.jpg"}
+                                alt="photo"
+                            />
+                        </Card>
                     </Grid>
-                </Box>
-                <Box m={2} pt={3} px={2} sx={{ bgcolor: '#cfe8fc', height: '400px', width: "100%" }}>
-                    <Grid container spacing ={2}>
-                        <Grid item xs={6}>
-                            <Card>
-                                <CardMedia 
-                                    component="img"
-                                    image={"https://scontent.fbeg1-1.fna.fbcdn.net/v/t1.6435-9/239468282_4772962376155021_5895707963065349972_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=a26aad&_nc_eui2=AeGka-vM_rkcTCzPvPgbU8a5UtFVQorY2cpS0VVCitjZyugPsr6WgVhcbFxbziP3zQYtbIM_S08V_lbKy-KRf-tw&_nc_ohc=E6MxPXtFia4AX8as8SR&_nc_ht=scontent.fbeg1-1.fna&oh=43fbc7be956fb2b2efc36205844d23a9&oe=616B8A87"}
-                                    alt="photo"
-                                />
-                            </Card>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography align="center" borderBottom={1}>
-                                <h1>Prints</h1>
-                            </Typography>
-                            <Typography>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </Typography>
-                        </Grid>
+                    <Grid item xs={6}>
+                        <Typography align="center" borderBottom={1}>
+                            <h1>Original Artwork</h1>
+                        </Typography>
+                        <Typography>
+                            <p>When creating original artwork, I always ensure that I use the highest quality and archival materials. This ensures that each piece will withstand the environment the artwork is placed in, protecting it from, light damage, fading and discolouration.</p>
+                            <p>If you would like to purchase any of my original art pieces, or have any questions, please feel free to contact me on email</p>
+                        </Typography>
                     </Grid>
-                </Box>
-            </Container>
+                </Grid>
+            </Box>
+            <Box m={2} pt={3} px={2} sx={{ bgcolor: '#cfe8fc', height: '500px', width: "100%" }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Typography align="center" borderBottom={1}>
+                            <h1>Gallery</h1>
+                        </Typography>
+                        <Typography>
+                            <p>Aside from creating pet portraits, I also enjoy producing other forms of diverse artwork, so if you're interested in seeing more, you can find these in the gallery where a substantial amount of my other creations can be viewed.</p>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardMedia 
+                                component="img"
+                                image={"/images/lion.jpg"}
+                                alt="photo"
+                            />
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Box m={2} pt={3} px={2} sx={{ bgcolor: '#cfe8fc', height: '500px', width: "100%" }}>
+                <Grid container spacing ={2}>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardMedia 
+                                component="img"
+                                image={"/images/Pets/17895221188716237.jpg"}
+                                alt="photo"
+                            />
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography align="center" borderBottom={1}>
+                            <h1>Prints</h1>
+                        </Typography>
+                        <Typography>
+                            <p>When creating original artwork, I always ensure that I use the highest quality and archival materials. This ensures that each piece will withstand the environment the artwork is placed in, protecting it from light damage, fading and discolouration.</p>
+                            <p>If you would like to purchese any of my original art pieces, or have any questions, please feel free to contact me on my email</p>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
     );
 };
 
